d00c9176494a496899354fb9031deed53d35a54c
Stephen Pennell <stepenn7@gmail.com>
2020-02-13 08:21:44 -0500

    Adds eslint for code linting

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'semi': 'error',
+    'quotes': ['error', 'single'],
+    'curly': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-multi-spaces': 'error',
+    'no-whitespace-before-property': 'error',
+    'brace-style': ['error', 'stroustrup'],
+    'indent': ['error', 2],
+  },
+};
